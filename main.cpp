14ffa9a81783c8647b5a7dc7adc33a4697a6a36e
Ali Aryani <lenesis@proton.me>
2025-09-17 11:02:23 +0330

    declare predictions

--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 int main() {
-   // in order to get different random numbers every time
-   // we run our program, we use time(0) and give it to srand()
-   srand(time(0));
-
+    // in order to get different random numbers every time
+    // we run our program, we use time(0) and give it to srand()
+    srand(time(0));
+    
+    char prediction0 [] {"a lot of things are running in the background!"};
+    char prediction1 [] {"a lot of empty beer bottles on your table!"};
+    char prediction2 [] {"you're partying too much with kids wearing weird clothes!"};
+    char prediction3 [] {"you're running away from something really scary."};
+    char prediction4 [] {"you're laughing you lungs out, I've never seen this before!"};
 
     return 0;
 }
