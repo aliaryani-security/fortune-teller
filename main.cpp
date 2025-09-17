cd2a1c6a3d57b0ec60f573e59c5e4dcba87e6558
Ali Aryani <lenesis@proton.me>
2025-09-17 10:57:23 +0330

    define srand(time(0))

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
+#include <ctime>
 #include <iostream>
 using namespace std;
 
 int main() {
-    
+   // in order to get different random numbers every time
+   // we run our program, we use time(0) and give it to srand()
+   srand(time(0));
+
 
     return 0;
 }
